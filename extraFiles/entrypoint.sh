3eb3346057a44915ffe031d8d147060126edc9d7
Ricardo Ruiz <rrcfesc@gmail.com>
2017-12-04 15:31:30 -0600

    Se agregan ant y se quita redis

--- extraFiles/entrypoint.sh
+++ extraFiles/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 chown -R $MAGENTO_USER:www-data/var/www/html
-chown -R $MAGENTO_USER:root /home/$MAGENTO_USER
+chown -R $MAGENTO_USER:$MAGENTO_USER /home/$MAGENTO_USER
 
 supervisord -n -c /etc/supervisord.conf
